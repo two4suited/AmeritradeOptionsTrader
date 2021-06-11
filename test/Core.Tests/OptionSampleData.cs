7caf4b12bff5e6c6340f02dc51e6368e09cbf614
Brian Sheridan <Brian.Sheridan@dhsoha.state.or.us>
2021-06-11 11:27:05 -0700

    Updated Option Verbiage

---
+++ test/Core.Tests/OptionSampleData.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using Core.Models;
+
+namespace Core.Tests
+{
+    public class OptionSampleData
+    {
+        public OptionSampleData()
+        {
+            Stock = new Stock() { Symbol="AMC" };            
+
+            for(int i=1;i<20;i++)
+            {
+                Stock.Options.Add(
+                    new Option(){
+                        ExpirationDate=new System.DateOnly(2022,1,22),
+                        Delta=1-(i*.1),
+                        StrikePrice=i,
+                        Premium=21.0-i
+                    });                  
+            }
+        }
+        public Stock Stock{get;set;}
+    }
+}
