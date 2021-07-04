f70dbae25b6473201d9a6f7a5453015028cb3421
Brian Sheridan <brian.sheridan@gmail.com>
2021-07-03 22:30:49 -0700

    Added Test to Check Mapping of Propeties in Ameritrade file

---
+++ test/External.Ameritrade.API.Tests/OptionChainTests.cs
@@ -0,0 +1,11 @@
+namespace External.Ameritrade.API.Tests
+{
+    public class OptionChainTests
+    {
+        [Fact]
+        public void LoadFile_ValidMaps()
+        {
+            
+        }
+    }
+}
